0e95aeba4956730eccf6875dddf0a98a2318808c
Pratyush Pallav <98553045+PratyushPallav@users.noreply.github.com>
2023-02-19 00:09:21 +0530

    Add files via upload

---
+++ Set_1.py
@@ -0,0 +1,29 @@
+a = {'1001':'ABC','1002':'BCD'}
+b = {'201':'AB','203':'CD'}
+#print(a)
+
+
+#print(a['1001'])
+
+
+
+#for i in a:
+    #print(a[i])
+
+
+
+#a = {'1003':'EFF'}
+#print(a)
+
+
+
+#a.update(b)
+#print(a)
+
+
+
+#del(a['1001'])
+#print(a)
+
+
+
